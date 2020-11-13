f8a1ffd365fc41f0cc54d485a79dc5ba7e57709b
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-13 06:02:23 -0800

    debug output

--- build-and-test-bindings.sh
+++ build-and-test-bindings.sh
@@ -4,8 +4,12 @@ cd /root || exit 1
 git clone --depth=1 https://github.com/appoptics/appoptics-bindings-node aob
 cd aob || exit 1
 
-# make sure install works
+# make sure production install works
 npm install --production --unsafe-perm
+
+# look around
+pwd
+ls -l
 
 # test
 npm install -g mocha
