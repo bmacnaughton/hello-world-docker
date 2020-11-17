692c5ef312a9456ffa40fa34c946872ab405624c
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-11-17 04:35:33 -0800

    fiddling

--- .github/actions/generic/build-and-test-bindings.sh
+++ .github/actions/generic/build-and-test-bindings.sh
@@ -10,45 +10,25 @@ cd aob || exit 1
 
 # make sure production install works
 if ! npm install --production --unsafe-perm; then
-  echo "::set-output name=install-prod::fail"
   error=true
-else
-  echo "::set-output name=install-prod::pass"
 fi
 
-rm -rf node_modules
+#rm -rf node_modules
 
-# install not production so testing works
-if ! npm install --unsafe-perm; then
-  echo "::set-output name=install-dev::fail"
-  error=true
-else
-  echo "::set-output name=install-dev::pass"
-fi
+# install so testing works
+#if ! npm install --unsafe-perm; then
+#  error=true
+#fi
 
-# mocha is installed globally in the development environment; it's
-# not a devDependency.
-npm install -g mocha || error=true
+# test
+#npm install -g mocha || error=true
+#npm test || error=true
 
-
-#
-# setup some vars that the test script will look for
-# and use.
-#
-export AOBT_PASS_COUNT="::set-output name=tests-passed-count::"
-export AOBT_FAIL_COUNT="::set-output name=tests-failed-count::"
-export AOBT_FAILED_TESTS="::set-output name=tests-failed::"
-
-# now execute the test
-if ! npm test; then
-  echo "::set-output name=install-dev::fail"
-  error=true
-else
-  echo "::set-output name=install-dev::pass"
-fi
 
 if [ -n "$error" ]; then
+  echo "::set-output name=result::fail"
   exit 1
 fi
 
+echo "::set-output name=result::pass"
 exit 0
